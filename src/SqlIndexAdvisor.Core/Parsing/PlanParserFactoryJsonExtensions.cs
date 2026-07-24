15caf612e8497a9f144d3b33a674af68071b5994
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 21:30:00 +0300

    SqlIndexAdvisor.Cli.dll

--- src/SqlIndexAdvisor.Core/Parsing/PlanParserFactoryJsonExtensions.cs
+++ src/SqlIndexAdvisor.Core/Parsing/PlanParserFactoryJsonExtensions.cs
@@ -4,65 +4,75 @@ using SqlIndexAdvisor.Core.Model;
 
 namespace SqlIndexAdvisor.Core.Parsing;
 
+/// <summary>
+/// Provides System.Text.Json serialization extensions for <see cref="PlanParserFactory"/>.
+/// </summary>
 public static class PlanParserFactoryJsonExtensions
 {
-    private static readonly JsonSerializerOptions s_jsonOptions = new()
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = true,
-    };
+	private static readonly JsonSerializerOptions s_jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+	};
 
-    /// <summary>
-    /// Converts a <see cref="PlanParserFactory"/> to JSON.
-    /// </summary>
-    /// <param name="value">The <see cref="PlanParserFactory"/> to convert.</param>
-    /// <param name="indented">Whether to format the JSON with indentation.</param>
-    /// <returns>The JSON representation of the <see cref="PlanParserFactory"/>.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <c>null</c>.</exception>
-    public static string ToJson(this PlanParserFactory value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes the <see cref="PlanParserFactory"/> to a JSON string.
+	/// </summary>
+	/// <param name="value">The factory instance to serialize.</param>
+	/// <param name="indented">Whether to indent the JSON for readability.</param>
+	/// <returns>A JSON representation of the factory.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+	public static string ToJson(this PlanParserFactory value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented ? s_jsonOptions : new JsonSerializerOptions(s_jsonOptions)
-        {
-            WriteIndented = false,
-        };
+		var options = indented
+			? new JsonSerializerOptions(s_jsonOptions) { WriteIndented = true }
+			: s_jsonOptions;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    /// <summary>
-    /// Converts JSON to a <see cref="PlanParserFactory"/>.
-    /// </summary>
-    /// <param name="json">The JSON to convert.</param>
-    /// <returns>The <see cref="PlanParserFactory"/> represented by the JSON, or <c>null</c> if the JSON is empty.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <c>null</c> or whitespace.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid.</exception>
-    public static PlanParserFactory? FromJson(string json)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(json);
+	/// <summary>
+	/// Deserializes a JSON string to a <see cref="PlanParserFactory"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized factory, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized into a <see cref="PlanParserFactory"/> instance.</exception>
+	public static PlanParserFactory? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        return JsonSerializer.Deserialize<PlanParserFactory>(json, s_jsonOptions);
-    }
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return null;
+		}
 
-    /// <summary>
-    /// Tries to convert JSON to a <see cref="PlanParserFactory"/>.
-    /// </summary>
-    /// <param name="json">The JSON to convert.</param>
-    /// <param name="value">The converted <see cref="PlanParserFactory"/>, or <c>null</c> if the conversion fails.</param>
-    /// <returns><c>true</c> if the conversion succeeds; otherwise, <c>false</c>.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <c>null</c> or whitespace.</exception>
-    public static bool TryFromJson(string json, out PlanParserFactory? value)
-    {
-        try
-        {
-            value = FromJson(json);
-            return value is not null;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+		return JsonSerializer.Deserialize<PlanParserFactory>(json, s_jsonOptions);
+	}
+
+	/// <summary>
+	/// Attempts to deserialize a JSON string to a <see cref="PlanParserFactory"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized factory, or <see langword="null"/> on failure.</param>
+	/// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+	public static bool TryFromJson(string json, out PlanParserFactory? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
+
+		try
+		{
+			value = FromJson(json);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
 }
